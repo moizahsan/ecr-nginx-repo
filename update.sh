c410d97afa02aa039d34a15faba6ad55bdabfac4
unknown <moizahsan99@gmail.com>
2022-06-21 15:04:48 +0500

    ecr-repo

---
+++ update.sh
@@ -0,0 +1,168 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+shopt -s nullglob
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+declare branches=(
+    "stable"
+    "mainline"
+)
+
+# Current nginx versions
+# Remember to update pkgosschecksum when changing this.
+declare -A nginx=(
+    [mainline]='1.21.6'
+    [stable]='1.22.0'
+)
+
+# Current njs versions
+declare -A njs=(
+    [mainline]='0.7.3'
+    [stable]='0.7.4'
+)
+
+# Current package patchlevel version
+# Remember to update pkgosschecksum when changing this.
+declare -A pkg=(
+    [mainline]=1
+    [stable]=1
+)
+
+declare -A debian=(
+    [mainline]='bullseye'
+    [stable]='bullseye'
+)
+
+declare -A alpine=(
+    [mainline]='3.15'
+    [stable]='3.16'
+)
+
+# When we bump njs version in a stable release we don't move the tag in the
+# mercurial repo.  This setting allows us to specify a revision to check out
+# when building alpine packages on architectures not supported by nginx.org
+# Remember to update pkgosschecksum when changing this.
+declare -A rev=(
+    [mainline]='688'
+    [stable]='696'
+)
+
+# Holds SHA512 checksum for the pkg-oss tarball produced by source code
+# revision/tag in the previous block
+# Used in alpine builds for architectures not packaged by nginx.org
+declare -A pkgosschecksum=(
+    [mainline]='a8ab6ff80ab67c6c9567a9103b52a42a5962e9c1bc7091b7710aaf553a3b484af61b0797dd9b048c518e371a6f69e34d474cfaaeaa116fd2824bffa1cd9d4718'
+    [stable]='fabf394af60d935d7c3f5e36db65dddcced9595fd06d3dfdfabbb77aaea88a5b772ef9c1521531673bdbb2876390cdea3b81c51030d36ab76cf5bfc0bfe79230'
+)
+
+get_packages() {
+    local distro="$1"
+    shift
+    local branch="$1"
+    shift
+    local perl=
+    local r=
+    local sep=
+
+    case "$distro:$branch" in
+    alpine*:*)
+        r="r"
+        sep="."
+        ;;
+    debian*:*)
+        sep="+"
+        ;;
+    esac
+
+    case "$distro" in
+    *-perl)
+        perl="nginx-module-perl"
+        ;;
+    esac
+
+    echo -n ' \\\n'
+    for p in nginx nginx-module-xslt nginx-module-geoip nginx-module-image-filter $perl; do
+        echo -n '        '"$p"'=${NGINX_VERSION}-'"$r"'${PKG_RELEASE} \\\n'
+    done
+    for p in nginx-module-njs; do
+        echo -n '        '"$p"'=${NGINX_VERSION}'"$sep"'${NJS_VERSION}-'"$r"'${PKG_RELEASE} \\'
+    done
+}
+
+get_packagerepo() {
+    local distro="${1%-perl}"
+    shift
+    local branch="$1"
+    shift
+
+    [ "$branch" = "mainline" ] && branch="$branch/" || branch=""
+
+    echo "https://nginx.org/packages/${branch}${distro}/"
+}
+
+get_packagever() {
+    local distro="${1%-perl}"
+    shift
+    local branch="$1"
+    shift
+    local suffix=
+
+    [ "${distro}" = "debian" ] && suffix="~${debianver}"
+
+    echo ${pkg[$branch]}${suffix}
+}
+
+generated_warning() {
+    cat <<__EOF__
+#
+# NOTE: THIS DOCKERFILE IS GENERATED VIA "update.sh"
+#
+# PLEASE DO NOT EDIT IT DIRECTLY.
+#
+__EOF__
+}
+
+for branch in "${branches[@]}"; do
+    for variant in \
+        alpine{,-perl} \
+        debian{,-perl}; do
+        echo "$branch: $variant"
+        dir="$branch/$variant"
+        variant="$(basename "$variant")"
+
+        [ -d "$dir" ] || continue
+
+        template="Dockerfile-${variant%-perl}.template"
+        {
+            generated_warning
+            cat "$template"
+        } >"$dir/Dockerfile"
+
+        debianver="${debian[$branch]}"
+        alpinever="${alpine[$branch]}"
+        nginxver="${nginx[$branch]}"
+        njsver="${njs[${branch}]}"
+        revver="${rev[${branch}]}"
+        pkgosschecksumver="${pkgosschecksum[${branch}]}"
+
+        packagerepo=$(get_packagerepo "$variant" "$branch")
+        packages=$(get_packages "$variant" "$branch")
+        packagever=$(get_packagever "$variant" "$branch")
+
+        sed -i.bak \
+            -e 's,%%ALPINE_VERSION%%,'"$alpinever"',' \
+            -e 's,%%DEBIAN_VERSION%%,'"$debianver"',' \
+            -e 's,%%NGINX_VERSION%%,'"$nginxver"',' \
+            -e 's,%%NJS_VERSION%%,'"$njsver"',' \
+            -e 's,%%PKG_RELEASE%%,'"$packagever"',' \
+            -e 's,%%PACKAGES%%,'"$packages"',' \
+            -e 's,%%PACKAGEREPO%%,'"$packagerepo"',' \
+            -e 's,%%REVISION%%,'"$revver"',' \
+            -e 's,%%PKGOSSCHECKSUM%%,'"$pkgosschecksumver"',' \
+            "$dir/Dockerfile"
+
+        cp -a entrypoint/*.sh "$dir/"
+
+    done
+done
